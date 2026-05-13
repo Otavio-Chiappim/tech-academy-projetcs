818c244008fcbddeb6ed06f8c922acad92d705b6
Otavio-Chiappim <otaviochiappim@gmail.com>
2026-05-13 10:50:46 -0400

    Update one_page_website.js

--- One-Page Website/one_page_website.js
+++ One-Page Website/one_page_website.js
@@ -1,10 +1,10 @@
 let currentImageIndex = 0;
 
 const images = [
-    "images/image1.jpg",
-    "images/image2.jpg",
-    "images/image3.jpg",
-    "images/image4.jpg"
+    "image1.jpg",
+    "image2.jpg",
+    "image3.jpg",
+    "image4.jpg"
 ];
 
 function openLightbox(index) {
