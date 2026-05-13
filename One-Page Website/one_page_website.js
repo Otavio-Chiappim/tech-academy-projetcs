014fef216b66d1aeb03f5a8f84d9d77be1dc87a9
Otavio-Chiappim <otaviochiappim@gmail.com>
2026-05-13 10:34:58 -0400

    JS (changed)

--- One-Page Website/one_page_website.js
+++ One-Page Website/one_page_website.js
@@ -1,8 +1,32 @@
-function openModal(image) {
+let currentImageIndex = 0;
+
+const images = [
+    "images/image1.jpg",
+    "images/image2.jpg",
+    "images/image3.jpg",
+    "images/image4.jpg"
+];
+
+function openLightbox(index) {
+    currentImageIndex = index;
     document.getElementById("lightbox").style.display = "block";
-    document.getElementById("lightbox-img").src = image.src;
+    document.getElementById("lightbox-img").src = images[currentImageIndex];
 }
 
-function closeModal() {
+function closeLightbox() {
     document.getElementById("lightbox").style.display = "none";
+}
+
+function changeImage(direction) {
+    currentImageIndex = currentImageIndex + direction;
+
+    if (currentImageIndex < 0) {
+        currentImageIndex = images.length - 1;
+    }
+
+    if (currentImageIndex >= images.length) {
+        currentImageIndex = 0;
+    }
+
+    document.getElementById("lightbox-img").src = images[currentImageIndex];
 }
